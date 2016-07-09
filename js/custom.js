8ed32dbcec8b683443564153af263181438a67dd
oregonjudo <oregonjudo@gmail.com>
2016-07-09 00:42:05 -0700

    dgs

--- js/custom.js
+++ js/custom.js
@@ -38,7 +38,7 @@ $(document).ready(function() {
     $('#map_iframe').addClass('scrolloff');
   });
   $(window).scroll(function() {
-    if ($(this).scrollTop() >= 1000) {        
+    if ($(this).scrollTop() >= 700) {        
         document.getElementById('scroll-top').style.display = "block";   
     } else {
         document.getElementById('scroll-top').style.display = "none";  
